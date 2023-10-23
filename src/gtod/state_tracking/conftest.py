cf603ce8bc94d3a8ee5d98779e3deab952ed8edc
Sherman Siu <s8siu@uwaterloo.ca>
2023-10-23 14:12:10 -0400

    Fix testdata dir location.

--- src/gtod/state_tracking/conftest.py
+++ src/gtod/state_tracking/conftest.py
@@ -8,6 +8,6 @@ TEST_DIR = "../testdata"
 @pytest.fixture
 def testdata_dir(request):
     # Get the path of the current test file
-    test_file = request.node.nodeid
+    test_file = request.module.__file__
     test_srcdir = pathlib.Path(test_file).parent
     return test_srcdir / TEST_DIR
