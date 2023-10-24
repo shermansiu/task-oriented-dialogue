7a1027a77470eee855704894c16f0252cfd3b24f
Sherman Siu <s8siu@uwaterloo.ca>
2023-10-24 01:00:39 -0400

    Fix pathing error.
    A test was failing because joining a path to the enum member was not using the enum member value.

--- src/gtod/util.py
+++ src/gtod/util.py
@@ -4,3 +4,6 @@ import enum
 class AutoNameEnum(str, enum.Enum):
     def _generate_next_value_(name, start, count, last_values):
         return name
+
+    def __str__(self):
+        return self.value
