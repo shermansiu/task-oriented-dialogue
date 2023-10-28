78939c181c48437c53a10dfa3af7772ac3f7a6d8
Sherman Siu <s8siu@uwaterloo.ca>
2023-10-28 11:51:54 -0400

    Update locations.

--- src/gtod/eval/sgd/evaluate.sh
+++ src/gtod/eval/sgd/evaluate.sh
@@ -1,4 +1,4 @@
 python evaluate.py \
 --dstc8_data_dir $code/magneto/datasets/dstc8-schema-guided-dialogue \
---prediction_dir $code/magneto/datasets/sgd/test/preds --eval_set test \
---output_metric_file ./metrics_test.json
+--prediction_dir $code/magneto/src/preds_parsed --eval_set test \
+--output_metric_file $code/magneto/src/metrics_test.json
